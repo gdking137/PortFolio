25fccfa45b5702202788cf7355490815afdc07c7
gdking137 <80569681+gdking137@users.noreply.github.com>
2022-08-28 23:48:13 -0400

    Fixed minor changes

--- main.js
+++ main.js
@@ -35,6 +35,12 @@ document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
+
 // Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
